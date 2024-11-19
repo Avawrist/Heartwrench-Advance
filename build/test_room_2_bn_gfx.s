859b71934dd883ab18385f9a58971fedd6d4574d
Avawrist <fullmetal.chase@gmail.com>
2024-11-19 15:44:26 -0800

    111924

--- build/test_room_2_bn_gfx.s
+++ build/test_room_2_bn_gfx.s
@@ -9,7 +9,7 @@
 @	+ regular map (flat), not compressed, 256x64 
 @	Total size: 96 + 192 + 32768 = 33056
 @
-@	Time-stamp: 2024-11-13, 20:02:16
+@	Time-stamp: 2024-11-13, 20:50:27
 @	Exported by Cearn's GBA Image Transmogrifier, v0.9.2
 @	( http://www.coranac.com/projects/#grit )
 @
