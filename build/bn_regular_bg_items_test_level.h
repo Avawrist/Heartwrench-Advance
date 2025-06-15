f502711fffac66dab43cbab3c5266149687aca32
[avawrist] <chase.michaeldavid@gmail.com>
2025-06-14 23:22:22 -0700

    06142025

--- build/bn_regular_bg_items_test_level.h
+++ build/bn_regular_bg_items_test_level.h
@@ -13,7 +13,7 @@
 //	+ regular map (flat), not compressed, 1280x640 
 //	Total size: 32 + 7232 + 1638400 = 1645664
 //
-//	Time-stamp: 2025-06-14, 19:57:21
+//	Time-stamp: 2025-06-14, 23:19:01
 //	Exported by Cearn's GBA Image Transmogrifier, v0.9.2
 //	( http://www.coranac.com/projects/#grit )
 //
