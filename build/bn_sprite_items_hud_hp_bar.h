bb1871782fc56ce453a748718608d75cd751f654
[avawrist] <chase.michaeldavid@gmail.com>
2025-07-08 22:01:25 -0700

    07082025 Moved Overdrive speedboost to Roll state from run speed. Reduced Overdrive to 1 level. Reduced max HP to 3 and starting HP to 1.

--- build/bn_sprite_items_hud_hp_bar.h
+++ build/bn_sprite_items_hud_hp_bar.h
@@ -7,12 +7,12 @@
 
 //======================================================================
 //
-//	hud_hp_bar_bn_gfx, 64x320@4, 
+//	hud_hp_bar_bn_gfx, 64x256@4, 
 //	+ palette 16 entries, not compressed
-//	+ 320 tiles not compressed
-//	Total size: 32 + 10240 = 10272
+//	+ 256 tiles not compressed
+//	Total size: 32 + 8192 = 8224
 //
-//	Time-stamp: 2025-07-08, 16:33:07
+//	Time-stamp: 2025-07-08, 21:56:11
 //	Exported by Cearn's GBA Image Transmogrifier, v0.9.2
 //	( http://www.coranac.com/projects/#grit )
 //
@@ -21,8 +21,8 @@
 #ifndef GRIT_HUD_HP_BAR_BN_GFX_H
 #define GRIT_HUD_HP_BAR_BN_GFX_H
 
-#define hud_hp_bar_bn_gfxTilesLen 10240
-extern const bn::tile hud_hp_bar_bn_gfxTiles[320];
+#define hud_hp_bar_bn_gfxTilesLen 8192
+extern const bn::tile hud_hp_bar_bn_gfxTiles[256];
 
 #define hud_hp_bar_bn_gfxPalLen 32
 extern const bn::color hud_hp_bar_bn_gfxPal[16];
@@ -34,7 +34,7 @@ extern const bn::color hud_hp_bar_bn_gfxPal[16];
 namespace bn::sprite_items
 {
     constexpr inline sprite_item hud_hp_bar(sprite_shape_size(sprite_shape::SQUARE, sprite_size::HUGE), 
-            sprite_tiles_item(span<const tile>(hud_hp_bar_bn_gfxTiles, 320), bpp_mode::BPP_4, compression_type::NONE, 5), 
+            sprite_tiles_item(span<const tile>(hud_hp_bar_bn_gfxTiles, 256), bpp_mode::BPP_4, compression_type::NONE, 4), 
             sprite_palette_item(span<const color>(hud_hp_bar_bn_gfxPal, 16), bpp_mode::BPP_4, compression_type::NONE));
 }
 
