d5a78363c5590a2c0fe7625039ef2e57e780e687
[avawrist] <chase.michaeldavid@gmail.com>
2025-07-10 17:17:59 -0700

    07102025

--- build/bn_sprite_items_candelabra.h
+++ build/bn_sprite_items_candelabra.h
@@ -12,7 +12,7 @@
 //	+ 256 tiles not compressed
 //	Total size: 32 + 8192 = 8224
 //
-//	Time-stamp: 2025-06-23, 15:30:36
+//	Time-stamp: 2025-07-10, 17:17:03
 //	Exported by Cearn's GBA Image Transmogrifier, v0.9.2
 //	( http://www.coranac.com/projects/#grit )
 //
