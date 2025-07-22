d27ac3dbe7c12968fd47d63e7ad82ab573f38df2
[avawrist] <chase.michaeldavid@gmail.com>
2025-07-22 16:04:44 -0700

    07222025

--- build/bn_regular_bg_items_ziggurat_1_painted_bg.h
+++ build/bn_regular_bg_items_ziggurat_1_painted_bg.h
@@ -9,11 +9,11 @@
 //
 //	ziggurat_1_painted_bg_bn_gfx, 1280x768@8, 
 //	+ palette 64 entries, not compressed
-//	+ 92 tiles (t|f reduced) not compressed
+//	+ 91 tiles (t|f reduced) not compressed
 //	+ regular map (flat), not compressed, 160x96 
-//	Total size: 128 + 5888 + 30720 = 36736
+//	Total size: 128 + 5824 + 30720 = 36672
 //
-//	Time-stamp: 2025-07-22, 15:53:37
+//	Time-stamp: 2025-07-22, 16:03:57
 //	Exported by Cearn's GBA Image Transmogrifier, v0.9.2
 //	( http://www.coranac.com/projects/#grit )
 //
@@ -22,8 +22,8 @@
 #ifndef GRIT_ZIGGURAT_1_PAINTED_BG_BN_GFX_H
 #define GRIT_ZIGGURAT_1_PAINTED_BG_BN_GFX_H
 
-#define ziggurat_1_painted_bg_bn_gfxTilesLen 5888
-extern const bn::tile ziggurat_1_painted_bg_bn_gfxTiles[184];
+#define ziggurat_1_painted_bg_bn_gfxTilesLen 5824
+extern const bn::tile ziggurat_1_painted_bg_bn_gfxTiles[182];
 
 #define ziggurat_1_painted_bg_bn_gfxMapLen 30720
 extern const bn::regular_bg_map_cell ziggurat_1_painted_bg_bn_gfxMap[15360];
@@ -38,7 +38,7 @@ extern const bn::color ziggurat_1_painted_bg_bn_gfxPal[64];
 namespace bn::regular_bg_items
 {
     constexpr inline regular_bg_item ziggurat_1_painted_bg(
-            regular_bg_tiles_item(span<const tile>(ziggurat_1_painted_bg_bn_gfxTiles, 184), bpp_mode::BPP_8, compression_type::NONE), 
+            regular_bg_tiles_item(span<const tile>(ziggurat_1_painted_bg_bn_gfxTiles, 182), bpp_mode::BPP_8, compression_type::NONE), 
             bg_palette_item(span<const color>(ziggurat_1_painted_bg_bn_gfxPal, 64), bpp_mode::BPP_8, compression_type::NONE),
             regular_bg_map_item(ziggurat_1_painted_bg_bn_gfxMap[0], size(160, 96), compression_type::NONE, 1, true));
 }
