6e21f17388a0a4e89349cdcd02c2d912815414f2
[avawrist] <chase.michaeldavid@gmail.com>
2025-08-02 13:29:45 -0700

    08022025 Player jump animation update

--- build/bn_sprite_items_player.h
+++ build/bn_sprite_items_player.h
@@ -12,7 +12,7 @@
 //	+ 1088 tiles not compressed
 //	Total size: 32 + 34816 = 34848
 //
-//	Time-stamp: 2025-08-01, 20:37:05
+//	Time-stamp: 2025-08-02, 13:26:58
 //	Exported by Cearn's GBA Image Transmogrifier, v0.9.2
 //	( http://www.coranac.com/projects/#grit )
 //
