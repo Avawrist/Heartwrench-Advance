ead33e769609e3b04f169700386cdb0b65cf9d3f
[avawrist] <chase.michaeldavid@gmail.com>
2025-08-07 19:49:39 -0700

    08072025

--- build/bn_regular_bg_items_ziggurat_1_object_bg.h
+++ build/bn_regular_bg_items_ziggurat_1_object_bg.h
@@ -13,7 +13,7 @@
 //	+ regular map (flat), not compressed, 640x320 
 //	Total size: 32 + 1472 + 409600 = 411104
 //
-//	Time-stamp: 2025-08-07, 16:53:14
+//	Time-stamp: 2025-08-07, 19:38:38
 //	Exported by Cearn's GBA Image Transmogrifier, v0.9.2
 //	( http://www.coranac.com/projects/#grit )
 //
