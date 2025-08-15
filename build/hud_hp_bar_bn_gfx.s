29e198e63c2f8d762f6bd4a3741bb44951afca89
[avawrist] <chase.michaeldavid@gmail.com>
2025-08-15 02:10:03 -0700

    08152025

--- build/hud_hp_bar_bn_gfx.s
+++ build/hud_hp_bar_bn_gfx.s
@@ -8,7 +8,7 @@
 @	+ 256 tiles not compressed
 @	Total size: 32 + 8192 = 8224
 @
-@	Time-stamp: 2025-08-15, 00:22:05
+@	Time-stamp: 2025-08-15, 02:03:29
 @	Exported by Cearn's GBA Image Transmogrifier, v0.9.2
 @	( http://www.coranac.com/projects/#grit )
 @
