c1014c912a360099ad1bffa6ce51471755053e4f
[avawrist] <chase.michaeldavid@gmail.com>
2025-08-29 13:06:46 -0700

    08292025

--- build/bn_sprite_items_hp_totem.h
+++ build/bn_sprite_items_hp_totem.h
@@ -12,7 +12,7 @@
 //	+ 64 tiles not compressed
 //	Total size: 32 + 2048 = 2080
 //
-//	Time-stamp: 2025-08-29, 02:23:07
+//	Time-stamp: 2025-08-29, 13:00:35
 //	Exported by Cearn's GBA Image Transmogrifier, v0.9.2
 //	( http://www.coranac.com/projects/#grit )
 //
