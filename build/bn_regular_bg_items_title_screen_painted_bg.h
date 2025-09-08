5781066de0108b164c117816fe14e21909fc892d
[avawrist] <chase.michaeldavid@gmail.com>
2025-09-08 15:48:30 -0700

    09082025

--- build/bn_regular_bg_items_title_screen_painted_bg.h
+++ build/bn_regular_bg_items_title_screen_painted_bg.h
@@ -9,11 +9,11 @@
 //
 //	title_screen_painted_bg_bn_gfx, 256x768@4, 
 //	+ palette 16 entries, not compressed
-//	+ 45 tiles (t|f|p reduced) not compressed
+//	+ 58 tiles (t|f|p reduced) not compressed
 //	+ regular map (flat), not compressed, 32x96 
-//	Total size: 32 + 1440 + 6144 = 7616
+//	Total size: 32 + 1856 + 6144 = 8032
 //
-//	Time-stamp: 2025-09-08, 15:33:30
+//	Time-stamp: 2025-09-08, 15:45:47
 //	Exported by Cearn's GBA Image Transmogrifier, v0.9.2
 //	( http://www.coranac.com/projects/#grit )
 //
@@ -22,8 +22,8 @@
 #ifndef GRIT_TITLE_SCREEN_PAINTED_BG_BN_GFX_H
 #define GRIT_TITLE_SCREEN_PAINTED_BG_BN_GFX_H
 
-#define title_screen_painted_bg_bn_gfxTilesLen 1440
-extern const bn::tile title_screen_painted_bg_bn_gfxTiles[45];
+#define title_screen_painted_bg_bn_gfxTilesLen 1856
+extern const bn::tile title_screen_painted_bg_bn_gfxTiles[58];
 
 #define title_screen_painted_bg_bn_gfxMapLen 6144
 extern const bn::regular_bg_map_cell title_screen_painted_bg_bn_gfxMap[3072];
@@ -38,7 +38,7 @@ extern const bn::color title_screen_painted_bg_bn_gfxPal[16];
 namespace bn::regular_bg_items
 {
     constexpr inline regular_bg_item title_screen_painted_bg(
-            regular_bg_tiles_item(span<const tile>(title_screen_painted_bg_bn_gfxTiles, 45), bpp_mode::BPP_4, compression_type::NONE), 
+            regular_bg_tiles_item(span<const tile>(title_screen_painted_bg_bn_gfxTiles, 58), bpp_mode::BPP_4, compression_type::NONE), 
             bg_palette_item(span<const color>(title_screen_painted_bg_bn_gfxPal, 16), bpp_mode::BPP_4, compression_type::NONE),
             regular_bg_map_item(title_screen_painted_bg_bn_gfxMap[0], size(32, 32), compression_type::NONE, 3, false));
 }
