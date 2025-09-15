83320e34c9eeec3eacb033fd3af5c51c5f929016
[avawrist] <chase.michaeldavid@gmail.com>
2025-09-14 17:23:47 -0700

    09142025

--- build/bn_regular_bg_items_troll_tolls_object_bg.h
+++ build/bn_regular_bg_items_troll_tolls_object_bg.h
@@ -13,7 +13,7 @@
 //	+ regular map (flat), not compressed, 640x320 
 //	Total size: 32 + 1120 + 409600 = 410752
 //
-//	Time-stamp: 2025-09-14, 17:07:15
+//	Time-stamp: 2025-09-14, 17:23:33
 //	Exported by Cearn's GBA Image Transmogrifier, v0.9.2
 //	( http://www.coranac.com/projects/#grit )
 //
