b259bd12293a61ca9ff9a75eaed0139dc04d74ac
Avawrist <fullmetal.chase@gmail.com>
2025-09-17 20:00:51 -0700

    09172025

--- build/_bn_audio_soundbank_bin.h
+++ build/_bn_audio_soundbank_bin.h
@@ -6,7 +6,7 @@
 extern const uint8_t _bn_audio_soundbank_bin[];
 extern const uint8_t _bn_audio_soundbank_bin_end[];
 #if __cplusplus >= 201103L
-static constexpr size_t _bn_audio_soundbank_bin_size=52445;
+static constexpr size_t _bn_audio_soundbank_bin_size=5487;
 #else
-static const size_t _bn_audio_soundbank_bin_size=52445;
+static const size_t _bn_audio_soundbank_bin_size=5487;
 #endif
